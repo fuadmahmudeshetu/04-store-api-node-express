8e5ae3ce7a145655641312c7816bffb2bdbceaf2
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-12 01:45:47 +0300

    Update products.js

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,6 +1,8 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res, next) => {
-    throw new Error('testing async errors')
-    res.status(200).json({ msg: 'products testing routes'})
+    const products = await Product.find({ feature:true })
+    res.status(200).json({ products })
 }
 
 const getAllProducts = async (req, res) => {
