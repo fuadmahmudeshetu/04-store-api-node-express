0e2ca43ec90ca807de7dedb49c3a19597e3d59d5
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-12 01:46:28 +0300

    Update products.js

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res, next) => {
-    const products = await Product.find({ feature:true })
+    const products = await Product.find({ featured:true })
     res.status(200).json({ products })
 }
 
