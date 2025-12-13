ae02ea22833d9f3310e782d74b43f47fe66efdf2
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-13 12:16:39 +0300

    Update products.js

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,20 +1,33 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res, next) => {
-    const products = await Product.find({ page : 2 })
+    const products = await Product.find().sort('-price')
+    
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured } = req.query
+    const { featured, company, name, sort } = req.query
     const queryObject = {}
 
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false
     }
 
-    console.log(queryObject)
-    const products = await Product.find(queryObject)
+    if(company) {
+        queryObject.company = company
+    }
+    
+    if(name) {
+        queryObject.name = { $regex: e, $options: 'i'}
+    }
+
+    // console.log(queryObject)
+    const result = Product.find(queryObject)
+    if (sort) {
+        products = products.sort()
+    }
+    const products = await result
     res.status(200).json({ products, nbHits: products.length})
 }
 
