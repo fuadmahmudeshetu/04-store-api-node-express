01ad6be7e2b2158950ca2d5ff19f3a1681962ea5
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-13 22:38:07 +0300

    Update products.js

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -2,15 +2,15 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res, next) => {
     const products = await Product
-    .find({})
-    .sort('name')
+    .find({ price: {$gt:30}})
+    .sort('price')
     .select('name price')
     
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort, fields } = req.query
+    const { featured, company, name, sort, fields, numericFilters } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -38,6 +38,23 @@ const getAllProducts = async (req, res) => {
     if (fields) {
         fieldsList = fields.split(',').join(' ')
         result = result.select(fieldsList)
+    }
+
+    if(numericFilters){
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$e',
+            '<': '$lt',
+            '<=': '$lt',
+        }
+
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g
+        let filters = numericFilters.replace(
+            regEx, (match) => `-${operatorMap[match]}-`
+        )
+
+        console.log(filters)
     }
 
     const page = Number(req.query.page) || 1
