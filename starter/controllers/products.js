cc0430d5fb43e9eb428153eefda0b46325e21a65
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-13 11:11:36 +0300

    Add featured filter to getAllProducts controller
    Updated the getAllProducts controller to support filtering products by the 'featured' query parameter. Also changed the static products query in getAllProductsStatic for demonstration purposes.

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,15 +1,21 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res, next) => {
-    const products = await Product.find({ name : 'vase table' })
+    const products = await Product.find({ page : 2 })
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const products = await Product.find(req.query)
-    console.log(req.query);
-    
-    res.status(200).json({ products, nbHits: products.length })
+    const { featured } = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
+    res.status(200).json({ products, nbHits: products.length})
 }
 
 module.exports = {
