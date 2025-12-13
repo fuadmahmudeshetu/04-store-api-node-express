80ce181fb91aff5cde5766ecff93871d9b854266
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-13 23:13:11 +0300

    Update products.js

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -40,23 +40,36 @@ const getAllProducts = async (req, res) => {
         result = result.select(fieldsList)
     }
 
-    if(numericFilters){
+    if (numericFilters) {
         const operatorMap = {
             '>': '$gt',
             '>=': '$gte',
-            '=': '$e',
+            '=': '$eq',
             '<': '$lt',
-            '<=': '$lt',
+            '<=': '$lte',
         }
 
-        const regEx = /\b(<|>|>=|=|<|<=)\b/g
+        const regEx = /(>=|<=|>|<|=)/g
+
         let filters = numericFilters.replace(
-            regEx, (match) => `-${operatorMap[match]}-`
+            regEx,
+            (match) => `-${operatorMap[match]}-`
         )
 
-        console.log(filters)
+        const options = ['price', 'rating']
+
+        filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split('-')
+
+            if (options.includes(field)) {
+                queryObject[field] = {
+                    [operator]: Number(value),
+                }
+            }
+        })
     }
 
+    console.log(queryObject); 
     const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || 10
     const skip = (page -1)*limit
