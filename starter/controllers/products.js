7889946efa6f5a61da19e16786a46a224faa91f1
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-13 21:54:10 +0300

    Update products.js

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -5,13 +5,12 @@ const getAllProductsStatic = async (req, res, next) => {
     .find({})
     .sort('name')
     .select('name price')
-    .limit(10)
     
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort, fields, limit } = req.query
+    const { featured, company, name, sort, fields } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -41,9 +40,11 @@ const getAllProducts = async (req, res) => {
         result = result.select(fieldsList)
     }
 
-    if (limit) {
-        result = result.limit(4)
-    }
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page -1)*limit
+
+    result = result.skip(skip).limit(limit)
     const products = await result
     res.status(200).json({ products, nbHits: products.length})
 }
